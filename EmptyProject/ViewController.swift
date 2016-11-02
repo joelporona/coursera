1a157aa342583bbcde2f1b88e76a315b5961d957
Joel P. Orona <joelp.orona@hotmail.com>
2016-11-02 06:52:31 -0600

    Initial Commit

---
+++ EmptyProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmptyProject
+//
+//  Created by Joel P. Orona on 11/2/16.
+//  Copyright © 2016 Joel P. Orona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
